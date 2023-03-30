b0f09a76dc9676f807848489d536df83f594964a
MatiiDiaz <matt03.06.01@gmail.com>
2023-03-29 23:29:25 -0300

    Version 1.0.2 Nueva vista creada sin enlazar

---
+++ app/src/main/java/com/example/proyecto/ListadeCompras.java
@@ -0,0 +1,13 @@
+package com.example.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadeCompras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listade_compras);
+    }
+}
