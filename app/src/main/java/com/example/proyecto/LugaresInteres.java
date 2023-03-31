cdefb9697d1cd1eff8c96de9c5a985848481b1d0
MatiiDiaz <matt03.06.01@gmail.com>
2023-03-30 21:24:02 -0300

    Version 1.1 Enlaces correctos

--- app/src/main/java/com/example/proyecto/LugaresInteres.java
+++ app/src/main/java/com/example/proyecto/LugaresInteres.java
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class ListadeCompras extends AppCompatActivity {
+public class LugaresInteres extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listade_compras);
+        setContentView(R.layout.activity_lugares_interes);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button buttonListaCompras;
+    Button buttonLugaresInteres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonListaCompras = (Button)findViewById(R.id.button2);
+        buttonLugaresInteres = (Button)findViewById(R.id.button3);
+        buttonListaCompras.setOnClickListener(this);
+        buttonLugaresInteres.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button2:
+                Intent listaCompras = new Intent(MainActivity.this,ListadeCompras.class);
+                startActivity(listaCompras);
+                break;
+            case R.id.button3:
+                Intent lugaresInteres = new Intent(MainActivity.this,LugaresInteres.class);
+                startActivity(lugaresInteres);
+                break;
+        }
     }
 }
