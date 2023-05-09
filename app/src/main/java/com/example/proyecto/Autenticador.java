44984e2e814ba1eae399b5a656ddb538a5531b43
MatiiDiaz <matt03.06.01@gmail.com>
2023-05-08 23:33:34 -0400

    Version 1.3. Corregido el error mas grande de la vida.

--- app/src/main/java/com/example/proyecto/Autenticador.java
+++ app/src/main/java/com/example/proyecto/Autenticador.java
@@ -1,11 +1,9 @@
 package com.example.proyecto;
 
 import android.content.Intent;
-import android.hardware.biometrics.BiometricPrompt;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
